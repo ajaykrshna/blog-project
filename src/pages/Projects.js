c6c5a81b30e3301f240fabf3bf967a8593175bb9
ajaykrshna <ajaykfan@gmail.com>
2023-06-22 18:10:31 +0530

    edited Projects

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,11 +1,22 @@
 import React from "react";
 import '../styles/projects.css'
+import proj1img from '../images/todo.png'
 
 function Projects() {
     return (
         <div className="projects">
             <h1 className="projects--heading">PROJECTS</h1>
             <h3 className="projects--subhead">List project</h3>
+            <div className="project1">
+                <img src={proj1img} />
+                <h1>To do list app</h1>
+                <p>Just for Fun</p>
+                <div className="project--tags">
+                    <div className="project--tag">Leadership</div>
+                    <div className="project--tag">Management</div>
+                    <div className="project--tag">Presentation</div>
+                </div>
+            </div>
         </div>
     )
 }
